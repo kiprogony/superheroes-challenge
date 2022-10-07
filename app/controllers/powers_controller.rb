7da402bfca1d3878b8c5a5adce7f52f847b9025a
Daryl Rogony <darylrogony@gmail.com>
2022-10-07 12:01:13 +0300

    Fit: add power model data

---
+++ app/controllers/powers_controller.rb
@@ -0,0 +1,16 @@
+class PowersController < ApplicationController
+
+  def index
+    powers = Power.all
+    render json: powers
+  end
+
+  def show
+    power = Power.find_by(id: params[:id])
+    if power
+      render json: power
+    else
+      render json: { error: "Power not found" }, status: :not_found
+    end
+  end
+end
